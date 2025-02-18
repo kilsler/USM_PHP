bf1016c4780fe36261cfcf6cd5154177fa9f8c69
kilsler <126207975+kilsler@users.noreply.github.com>
2025-02-18 10:00:01 +0200

    Add files via upload

---
+++ lab1/index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<?php
+
+ echo("Привет, мир!");
+ echo "Hello, World with echo!";
+ print "Hello, World with print!";
+
+$days = 228;
+$num2 = "Все возвращаются на работу!";
+echo $days.' '.$num2;
+echo "<br>{$days} {$num2}"
+?>
+
+</div>
+</body>
+</html>
